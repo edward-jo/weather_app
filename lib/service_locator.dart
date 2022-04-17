23b983ddbd5374ff46e19bf071ca7f7396eb656a
Edward Jo <helloedwardjo@gmail.com>
2022-04-17 15:29:07 +0900

    Test city list loading on start

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:developer' as developer;
+
+import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
+import 'package:weather_app/services/weather/city.dart';
 import 'package:weather_app/services/worldclock/worldclock_service.dart';
 import 'package:weather_app/services/worldclock/worldclock_service_impl.dart';
 
@@ -6,4 +11,16 @@ final serviceLocator = GetIt.instance;
 
 void setupServiceLocator() {
   serviceLocator.registerSingleton<WorldClockService>(WorldClockServiceImpl());
+
+  // XXX: Test loading city list
+  serviceLocator.registerSingletonAsync<List<City>>(() async {
+    // TODO: Handle exception
+    final jsonStr = await rootBundle.loadString('assets/simple.city.list.json');
+    final cityListJson = json.decode(jsonStr);
+    final cityList = List<City>.from(cityListJson.map((e) => City.fromJson(e)));
+    for (final c in cityList) {
+      developer.log(c.toJson().toString());
+    }
+    return cityList;
+  });
 }
