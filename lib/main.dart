2cb61cd4d1028c4397518fc1417bca37a5099d34
Edward Jo <helloedwardjo@gmail.com>
2022-04-17 18:01:32 +0900

    Wait for service locator is ready and expose WeatherViewModel

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_app/service_locator.dart';
 import 'package:weather_app/view_models/datetime_viewmodel.dart';
+import 'package:weather_app/view_models/weather_viewmodel.dart';
 import 'package:weather_app/views/weather/weather_screen.dart';
 
 void main() async {
@@ -21,19 +22,50 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider.value(
-          value: serviceLocator<DateTimeViewModel>(),
-        ),
-      ],
-      child: MaterialApp(
-        title: 'Studypie',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const WeatherScreen(),
-      ),
-    );
+    return FutureBuilder(
+        future: serviceLocator.allReady(),
+        builder: (context, snapshot) {
+          /// Show indicator until services are ready
+          if (snapshot.connectionState != ConnectionState.done) {
+            return Container(
+              color: Colors.white,
+              child: const Center(
+                child: CircularProgressIndicator(strokeWidth: 2.0),
+              ),
+            );
+          }
+
+          /// Show failure message if failed to init services
+          if (snapshot.hasError) {
+            return Container(
+              color: Colors.white,
+              child: Center(
+                child: Text(
+                  snapshot.error.toString(),
+                  textDirection: TextDirection.ltr,
+                ),
+              ),
+            );
+          }
+
+          /// Start app
+          return MaterialApp(
+            title: 'Studypie',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: MultiProvider(
+              providers: [
+                ChangeNotifierProvider.value(
+                  value: serviceLocator<DateTimeViewModel>(),
+                ),
+                ChangeNotifierProvider.value(
+                  value: serviceLocator<WeatherViewModel>(),
+                ),
+              ],
+              child: const WeatherScreen(),
+            ),
+          );
+        });
   }
 }
