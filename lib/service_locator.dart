4a7342604ffc01e712fd4b6b7f71a420ca9a84fa
Edward Jo <helloedwardjo@gmail.com>
2022-04-17 16:04:23 +0900

    Create WeatherService singleton before runApp

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -3,7 +3,11 @@ import 'dart:developer' as developer;
 
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
+import 'package:weather/weather.dart';
+import 'package:weather_app/secure.dart';
 import 'package:weather_app/services/weather/city.dart';
+import 'package:weather_app/services/weather/weather_service.dart';
+import 'package:weather_app/services/weather/weather_service_impl.dart';
 import 'package:weather_app/services/worldclock/worldclock_service.dart';
 import 'package:weather_app/services/worldclock/worldclock_service_impl.dart';
 
@@ -11,6 +15,11 @@ final serviceLocator = GetIt.instance;
 
 void setupServiceLocator() {
   serviceLocator.registerSingleton<WorldClockService>(WorldClockServiceImpl());
+
+  serviceLocator.registerSingleton<WeatherService>(() {
+    final wf = WeatherFactory(weatherApiKey);
+    return WeatherServiceImpl(wf);
+  }());
 
   // XXX: Test loading city list
   serviceLocator.registerSingletonAsync<List<City>>(() async {
