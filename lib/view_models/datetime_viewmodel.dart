2e94c266eac0b4268969af9280f7d2477a589841
Edward Jo <helloedwardjo@gmail.com>
2022-04-19 14:17:17 +0900

    Check if internet is available before getting date and weather data

--- lib/view_models/datetime_viewmodel.dart
+++ lib/view_models/datetime_viewmodel.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/models/datetime_model.dart';
 import 'package:weather_app/services/worldclock/worldclock_service.dart';
+import 'package:weather_app/view_models/mixins/internet_checking.dart';
 
-class DateTimeViewModel extends ChangeNotifier {
+class DateTimeViewModel extends ChangeNotifier with InternetChecking {
   DateTimeViewModel(this._service);
 
   final WorldClockService _service;
 
   Future<DateTimeModel> fetchCurrentDateTime() async {
+    await makeErrorInInternetNotAvailableCase();
     var ret = await _service.getCurrentDateTime();
     return DateTimeModel(ret.currentDateTime);
   }
